13ae7d60bb3efb93f7d8f7143c58014602601c54
Thomas G. Kristensen <thomas.g.kristensen@gmail.com>
2013-09-15 13:32:03 +0100

    Add d3 visualisation of states cooling off

--- src/cljs/example.cljs
+++ src/cljs/example.cljs
@@ -1,8 +1,10 @@
 (ns example
-  (:use [propaganda.system :only [make-system add-value constant get-value]]
+  (:use [propaganda.system :only [make-system add-value constant get-value stabile?]]
         [propaganda.values :only [default-merge default-contradictory?]]
-        [propaganda.intervals.common :only [make-interval]]
-        [propaganda.intervals.system :only [extend-merge quadratic product]]))
+        [propaganda.intervals.common :only [make-interval interval?]]
+        [propaganda.intervals.system :only [extend-merge quadratic product]])
+  (:require [clojure.string :as string]
+            [example.components.Visualisation]))
 
 (defn fall-duration
   [system t h]
@@ -24,30 +26,61 @@
         (product s-ba ratio h-ba)
         (product s ratio h))))
 
-(defn test
+(defn system-chain
+  [system]
+  (when system
+    (cons system
+          (system-chain (-> system meta :prev)))))
+
+(defn value-map
+  [system]
+  (let [values (:values system)
+        ks (keys values)
+        kw-keys (filter keyword? ks)]
+    (select-keys values kw-keys)))
+
+(defn pretty
+  [v]
+  (cond
+   (interval? v) (format "[%.4f, %.4f]" (:lo v) (:hi v))
+   :else (str v)))
+
+(defn value-str
+  [m]
+  (string/join
+   "\n"
+   (for [[k v] (sort m)]
+     (format "%20s %s" (name k) (pretty v)))))
+
+(defn presentation-map
+  [system]
+  {:values (value-str (value-map system))
+   :stabile (stabile? system)})
+
+(defn building-height
   []
   (let [custom-merge (doto (default-merge) extend-merge)
         system (make-system custom-merge (default-contradictory?))
-        result-system (-> system
+        audit-system (with-meta system {:audit? true})]
+    (-> audit-system
 
-                          (fall-duration :fall-time :building-height)
-                          (add-value :fall-time (make-interval 2.9 3.1))
+        (fall-duration :fall-time :building-height)
+        (add-value :fall-time (make-interval 2.9 3.1))
 
-                          (similar-triangles :barometer-shadow :barometer-height
-                                             :building-shadow :building-height)
-                          (add-value :building-shadow (make-interval 54.9 55.1))
-                          (add-value :barometer-height (make-interval 0.3 0.32))
-                          (add-value :barometer-shadow (make-interval 0.36 0.37))
+        (similar-triangles :barometer-shadow :barometer-height
+                           :building-shadow :building-height)
+        (add-value :building-shadow (make-interval 54.9 55.1))
+        (add-value :barometer-height (make-interval 0.3 0.32))
+        (add-value :barometer-shadow (make-interval 0.36 0.37))
 
-                          (add-value :building-height 45.0))]
-
-    [(get-value result-system :building-height)
-     (get-value result-system :building-shadow)
-     (get-value result-system :barometer-height)
-     (get-value result-system :barometer-shadow)
-     (get-value result-system :fall-time)]))
+        (add-value :building-height 45.0))))
 
 (defn ^:export init
   []
-  (.log js/console "Init")
-  (.log js/console (pr-str (test))))
+  (let [result-system (building-height)
+        chain (reverse (system-chain result-system))
+        visualisation (example.components.Visualisation. "body" 960 300)]
+    (.log js/console (clj->js (map presentation-map chain)))
+    (.update visualisation (clj->js (map presentation-map chain)))
+
+    ))
---
+++ src/js/example/components/Visualisation.js
@@ -0,0 +1,64 @@
+goog.provide('example.components.Visualisation');
+
+example.components.Visualisation = function(elem, width, height) {
+  this.elem = elem;
+  this.width = width;
+  this.height = height;
+
+  this.svg = d3.select(this.elem).append("svg")
+    .attr("width", this.width)
+    .attr("height", this.height)
+    .append("g");
+
+  this.svg.append("rect")
+    .attr("width", this.width)
+    .attr("height", this.height);
+
+  this.node = this.svg.selectAll(".node");
+};
+
+example.components.Visualisation.prototype.update = function(states) {
+
+  console.log(states);
+
+  var scale = d3.scale.linear()
+                .domain([0, states.length - 1])
+                .range([90, this.width - 90]);
+
+  this.node = this.node.data(states);
+
+  this.node.enter()
+    .insert("circle", ".node")
+    .attr("class", "node")
+    .classed("stabile",   function(d) { return  d.stabile; })
+    .classed("unstabile", function(d) { return !d.stabile; })
+    .attr("r", function(d) { return d.stabile ? 25 : 15; })
+    .attr("cx", function(d, i) { return scale(i); })
+    .attr("cy", this.height * 3 / 2)
+    .on("mousemove", function(d) {
+        var xPosition = d3.select(this).attr("cx") - 170 / 2;
+        var yPosition = d3.select(this).attr("cy");
+
+        d3.select("#tooltip")
+            .style("left", xPosition + "px")
+            .style("top", yPosition + "px");
+        d3.select("#tooltip #values")
+            .text(d.values);
+        d3.select("#tooltip").classed("hidden", false);
+    })
+    .on("mouseout", function() {
+        d3.select("#tooltip").classed("hidden", true);
+    });
+
+  this.node.exit()
+    .transition()
+    .duration(1000)
+    .attr("cy", this.height * 3 /2)
+    .each("end", function() { d3.select(this).remove(); });
+
+  this.node
+    .transition()
+    .duration(1000)
+    .attr("cx", function(d, i) { return scale(i); })
+    .attr("cy", this.height / 2);
+};
